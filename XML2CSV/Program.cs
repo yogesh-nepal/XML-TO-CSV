34abf5545646342baca737385768f6ad1cd73d2f
Yogesh Nepal <yogesn2@gmail.com>
2023-04-29 20:59:45 +0545

    xml2pdf

--- XML2CSV/Program.cs
+++ XML2CSV/Program.cs
@@ -1,5 +1,10 @@
-﻿using System.Data;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Collections.Generic;
+using System.Data;
+using System.Text.RegularExpressions;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace XML2CSV
 {
@@ -7,14 +12,140 @@ namespace XML2CSV
     {
         static void Main(string[] args)
         {
-            string fileNamePath = "D://[PROJECT]//XML2CSV//XML2CSV//XML//TestData.xml";
-            string outputFile = "D://[PROJECT]//XML2CSV//XML2CSV//Output//TestData.csv";
+            string fileNamePath = "D://[PROJECT]//XML2CSV//XML2CSV//XML//1066214.xml";
+            string outputFile = "D://[PROJECT]//XML2CSV//XML2CSV//Output//TestData5.pdf";
+
+            ConvertToPDF(fileNamePath, outputFile);
 
             //Using XML exported from EXCEL
-            ConvertToCSV(fileNamePath, outputFile, true);
+            //ConvertToCSV(fileNamePath, outputFile, true);
 
             //Using default XML 
-            ConvertToCSV(fileNamePath, outputFile, false);
+            //ConvertToCSV(fileNamePath, outputFile, false);
+
+        }
+
+        private static void ConvertToPDF(string fileNamePath, string outputFile)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileNamePath);
+            Document pdfDoc = new Document();
+            PdfWriter.GetInstance(pdfDoc, new FileStream(outputFile, FileMode.Create));
+            pdfDoc.Open();
+
+            var nodes = doc.ChildNodes;
+            List<XmlNode> filteredData = nodes.Cast<XmlNode>().ToList();
+            var stuffToRemove = nodes.Cast<XmlNode>().ToList().Where(x => x.LocalName.ToLower().Contains("xml")).ToList();
+            foreach (var item in stuffToRemove)
+            {
+                filteredData.Remove(item);
+            }
+            XDocument xdoc = XDocument.Load(fileNamePath);
+            var root = xdoc.Root;
+            var xElements = xdoc.Descendants().OfType<XElement>().FirstOrDefault();
+            Font font = FontFactory.GetFont("Arial", 16, Font.NORMAL | Font.UNDERLINE | Font.BOLD, BaseColor.BLACK);
+            Paragraph title = new Paragraph(GetStringFromLowerCamelCase(xElements.Name.LocalName), font);
+            title.Alignment = Element.ALIGN_LEFT;
+            pdfDoc.Add(title);
+            Console.WriteLine(xElements.Name.LocalName);
+            CreatePdf(xElements, pdfDoc);
+            pdfDoc.Close();
+            Console.ReadLine();
+        }
+
+        private static void CreatePdf(XElement xElements, Document pdfDoc)
+        {
+            Font font = FontFactory.GetFont("Arial", 12, Font.NORMAL, BaseColor.BLACK);
+
+            if (xElements.Elements().ToList().Count() > 0)
+            {
+                foreach (XElement element in xElements.Elements().ToList())
+                {
+                    var hasChild = element.Elements().ToList().Count() > 0;
+                    if (hasChild)
+                    {
+                        string text = element.Name.LocalName;
+                        Console.WriteLine(text);
+                        pdfDoc.Add(new Paragraph(" ", font));
+                        pdfDoc.Add(new Paragraph(GetStringFromLowerCamelCase(text), font));
+                        pdfDoc.Add(new Paragraph(" ", font));
+                        var hasAttributes = element.HasAttributes;
+                        if (hasAttributes)
+                        {
+                            string iText = "";
+                            string textName = element.Name.LocalName;
+                            var attributes = element.Attributes().ToList();
+                            foreach (var item in attributes)
+                            {
+                                var attrValue = GetStringFromLowerCamelCase(item.Name.LocalName) + (!string.IsNullOrEmpty(item.Value) ? " : " + item.Value : string.Empty);
+                                if (iText != "")
+                                {
+                                    iText += ", " + attrValue;
+                                }
+                                else
+                                {
+                                    iText = attrValue;
+                                }
+                            }
+                            Font dataFont = FontFactory.GetFont("Arial", 12, Font.NORMAL, BaseColor.BLACK);
+                            pdfDoc.Add(new Paragraph(GetStringFromLowerCamelCase(textName) + " | " + iText, dataFont));
+                            Console.WriteLine(textName + " | " + iText);
+                        }
+                        //pdfDoc.Add(new Paragraph(text, font));
+                        CreatePdf(element, pdfDoc);
+                    }
+                    else
+                    {
+                        var hasAttributes = element.HasAttributes;
+                        string iText = "";
+                        if (hasAttributes)
+                        {
+                            string text = element.Name.LocalName;
+                            var attributes = element.Attributes().ToList();
+                            foreach (var item in attributes)
+                            {
+                                var attrValue = GetStringFromLowerCamelCase(item.Name.LocalName) + (!string.IsNullOrEmpty(item.Value) ? " : " + item.Value : string.Empty);
+                                if (iText != "")
+                                {
+                                    iText += ", " + attrValue;
+                                }
+                                else
+                                {
+                                    iText = attrValue;
+                                }
+                            }
+                            pdfDoc.Add(new Paragraph(GetStringFromLowerCamelCase(text) + " | " + iText, font));
+                            Console.WriteLine(text + " | " + iText);
+                        }
+                        else
+                        {
+                            string innerText = GetStringFromLowerCamelCase(element.Name.LocalName) + (!string.IsNullOrEmpty(element.Value) ? " : " + element.Value : string.Empty);
+                            if (element.Value.Contains("data:image/png;base64"))
+                            {
+                                Regex regex = new Regex(@"^data:image/(?<mediaType>[^;]+);base64,(?<data>.*)");
+                                Match match = regex.Match(element.Value);
+                                Image image = Image.GetInstance(Convert.FromBase64String(match.Groups["data"].Value));
+                                pdfDoc.Add(image);
+                            }
+                            else
+                            {
+                                if (!string.IsNullOrEmpty(element.Value))
+                                {
+                                    var keepTogether = new PdfPTable(1)
+                                    {
+                                        KeepTogether = true,
+                                        WidthPercentage = 100f
+                                    };
+                                    keepTogether.DefaultCell.Border = Rectangle.NO_BORDER;
+                                    keepTogether.AddCell(new Paragraph(innerText, font));
+                                    pdfDoc.Add(keepTogether);
+                                    Console.WriteLine(innerText);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private static void ConvertToCSV(string fileNamePath, string outputFile, bool excelExported)
@@ -88,6 +219,18 @@ namespace XML2CSV
 
             // Otherwise, return the original field value
             return field;
+        }
+        static string GetStringFromLowerCamelCase(string input)
+        {
+
+            // Split the string into separate words using a regular expression
+            string[] words = Regex.Split(input, @"(?<!^)(?=[A-Z])");
+
+            // Capitalize the first letter of each word and join them together
+            string output = string.Join(" ", words.Select(w => char.ToUpper(w[0]) + w.Substring(1)));
+
+            return output;
+
         }
     }
 }
